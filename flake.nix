8b647f62ccd18abb8e523175f0c65ba5c4f02ca9
axshb <arsh.bhole@gmail.com>
2026-02-16 02:53:21 +1100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "16bases angular";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    devShells.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+
+        # node
+        nodejs_24
+        nodePackages.typescript
+        nodePackages.typescript-language-server
+        nodePackages.vscode-langservers-extracted
+      ];
+    };
+  };
+}
+
+
